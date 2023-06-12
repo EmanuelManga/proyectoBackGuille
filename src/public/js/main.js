5af82bd9abe9e1e8955d62d9f7eaefcce5a27efe
emanuel <emanuel_mangani@yahoo.com>
2023-06-12 19:27:35 -0300

    boton carrito

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -36,6 +36,7 @@ const login = () => {
             success: function (response) {
                 console.log(response);
                 guardarDatosEnSessionStorage(result.value.login, response.data._id);
+                loginStyling(result.value.login, response.data._id);
                 // alert("¡Formulario enviado con éxito!");
                 // Manejar la respuesta del servidor
             },
@@ -52,3 +53,42 @@ const guardarDatosEnSessionStorage = (usuario, cartId) => {
     sessionStorage.setItem("usuario", usuario);
     sessionStorage.setItem("cartId", cartId);
 };
+
+const loginStyling = (user, cid) => {
+    const btnLogIn = document.getElementById("btnLogIn");
+    const aCarrito = document.getElementById("a-carrito");
+    const dropdownMenuButton2 = document.getElementById("dropdownMenuButton2");
+    const contenedorUsuario = document.getElementById("contenedor-usuario");
+    aCarrito.setAttribute("href", "http://localhost:8080/carts/" + cid);
+    btnLogIn.style.display = "none";
+    contenedorUsuario.style.display = "";
+    dropdownMenuButton2.textContent = user;
+};
+
+const logOutStyling = () => {
+    sessionStorage.clear();
+    const btnLogIn = document.getElementById("btnLogIn");
+    const aCarrito = document.getElementById("a-carrito");
+    const dropdownMenuButton2 = document.getElementById("dropdownMenuButton2");
+    const contenedorUsuario = document.getElementById("contenedor-usuario");
+    aCarrito.setAttribute("href", "");
+    btnLogIn.style.display = "";
+    contenedorUsuario.style.display = "none";
+    dropdownMenuButton2.textContent = "";
+};
+
+function verificarExistencia() {
+    const usuarioExiste = sessionStorage.getItem("usuario");
+    const cartIdExiste = sessionStorage.getItem("cartId");
+
+    console.log("usuarioExiste,cartIdExiste", usuarioExiste, cartIdExiste);
+    if (usuarioExiste != null && cartIdExiste != null) {
+        console.log("entre");
+        loginStyling(usuarioExiste, cartIdExiste);
+    } else {
+        console.log("no entre");
+        logOutStyling();
+    }
+}
+
+verificarExistencia();
