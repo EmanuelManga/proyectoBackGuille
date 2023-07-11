abb757c8244c69c97157873881245f5ebe66c5fb
emanuel.mangani <emanuel.mangani@vicentelopez.gov.ar>
2023-07-11 13:39:19 -0300

    agregando api/sessions/current

--- src/routers/session.router.js
+++ src/routers/session.router.js
@@ -1,6 +1,9 @@
 import passport from "passport";
 import express from "express";
+import { UserService } from "../services/users.service.js";
 export const sessionsRouter = express.Router();
+
+const Service = new UserService();
 
 sessionsRouter.get("/github", passport.authenticate("github", { scope: ["user:email"] }));
 
@@ -11,4 +14,24 @@ sessionsRouter.get("/githubcallback", passport.authenticate("github", { failureR
 
 sessionsRouter.get("/show", (req, res) => {
     return res.send(JSON.stringify(req.session));
+});
+
+sessionsRouter.get("/current", async (req, res) => {
+    // console.log("req.session.email", req.session.email);
+    try {
+        const email = req.session.email;
+        const getUser = await Service.getByEmail(email);
+        return res.status(201).json({
+            status: "success",
+            msg: "Current User",
+            data: getUser,
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            status: "error",
+            msg: "something went wrong :(",
+            data: {},
+        });
+    }
 });
