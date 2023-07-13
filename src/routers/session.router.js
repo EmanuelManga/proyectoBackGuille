bf4f2f32f911cec0b6bdfd875e1d94a8b100ffc5
emanuel <emanuel_mangani@yahoo.com>
2023-07-13 18:58:15 -0300

    nose que pasa

--- src/routers/session.router.js
+++ src/routers/session.router.js
@@ -15,3 +15,23 @@ sessionsRouter.get("/githubcallback", passport.authenticate("github", { failureR
 sessionsRouter.get("/show", (req, res) => {
     return res.send(JSON.stringify(req.session));
 });
+
+sessionsRouter.get("/current", async (req, res) => {
+    // console.log("req.session.email", req.session.email);
+    try {
+        const email = req.session.email;
+        const getUser = await Service.getByEmail(email);
+        return res.status(201).json({
+            status: "success",
+            msg: "Current User",
+            data: getUser,
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            status: "error",
+            msg: "something went wrong :(",
+            data: {},
+        });
+    }
+});
